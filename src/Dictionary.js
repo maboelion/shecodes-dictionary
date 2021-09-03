69bb85d2f951cfbbad98223cfb40ddf27b09882d
maboelion <mareike.boettcher@gmail.com>
2021-09-03 14:52:38 +0200

    axios imported & API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
 	const [keyword, setKeyword] = useState(null);
 
+	function handleResponse(response) {
+		console.log(response.data[0]);
+	}
+
 	function searchKeyword(event) {
 		event.preventDefault();
-		alert(`Searching for ${keyword}`);
+
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+		axios.get(apiUrl).then(handleResponse);
 	}
 
 	function updateKeyword(event) {
