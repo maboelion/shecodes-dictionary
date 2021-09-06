07517e37033a181262c9f24de50ab79c5c7d15e5
maboelion <mareike.boettcher@gmail.com>
2021-09-06 14:36:18 +0200

    synonyms added

---
+++ src/Synonyms.js
@@ -0,0 +1,16 @@
+import { func } from "prop-types";
+import React from "react";
+
+export default function Synonyms(props) {
+	if (props.synonyms) {
+		return (
+			<div className="Synonyms">
+				<ul>
+					{props.synonyms.map(function (synonym, index) {
+						return <li key="index">{synonym}</li>;
+					})}
+				</ul>
+			</div>
+		);
+	} else return null;
+}
