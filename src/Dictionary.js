121b419b1926f06b65074e01adbcd3a8c938b318
maboelion <mareike.boettcher@gmail.com>
2021-09-11 15:28:44 +0200

    photos (+API) added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
 	const [keyword, setKeyword] = useState("sunset");
 	const [results, setResults] = useState(null);
 	const [loaded, setLoaded] = useState(false);
+	const [photos, setPhotos] = useState(null);
 
 	function handleResponse(response) {
 		setResults(response.data[0]);
 	}
 
+	function handlePexelsResponse(response) {
+		setPhotos(response.data.photos);
+	}
+
 	function search() {
 		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 		axios.get(apiUrl).then(handleResponse);
+
+		const apiKey = "563492ad6f91700001000001f55dd99188bc4556ab3bd71cb7f85c19";
+		let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
+		let headers = { Authorization: `Bearer ${apiKey}` };
+		axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
 	}
 
 	function handleSubmit(event) {
@@ -45,8 +56,13 @@ export default function Dictionary() {
 						/>
 					</form>
 					<span>e.g. alpaca, sunrise, dream, ...</span>
+					<br />
+					<span>
+						note: you can't use names like "London", "Andes", "Hercules" etc.
+					</span>
 				</section>
 				<Results results={results} />
+				<Photos photos={photos} />
 			</div>
 		);
 	} else {
